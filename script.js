c0e723245bfec9ac27e71281f990d4a035050440
Dodorion <yusuf.aydinli@web.de>
2025-12-28 14:47:15 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("todoInput");
+const button = document.getElementById("addBtn");
+const list = document.getElementById("todoList");
+
+button.addEventListener("click", () => {
+  const text = input.value;
+
+  if (text === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = text;
+
+  list.appendChild(li);
+  input.value = "";
+});
