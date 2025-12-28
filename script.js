bf0dbfa1bd6404b8628137f85777ac8dc64c8748
Dodorion <yusuf.aydinli@web.de>
2025-12-28 14:49:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const input = document.getElementById("todoInput");
-const button = document.getElementById("addBtn");
-const list = document.getElementById("todoList");
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("todoInput");
+  const button = document.getElementById("addBtn");
+  const list = document.getElementById("todoList");
 
-button.addEventListener("click", () => {
-  const text = input.value;
+  button.addEventListener("click", () => {
+    const text = input.value;
+    if (text === "") return;
 
-  if (text === "") return;
+    const li = document.createElement("li");
+    li.textContent = text;
 
-  const li = document.createElement("li");
-  li.textContent = text;
-
-  list.appendChild(li);
-  input.value = "";
+    list.appendChild(li);
+    input.value = "";
+  });
 });
